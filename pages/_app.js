b30f9cd0b39a0c2ac3e65fa30982f4dbd75ec797
this.Konrad <konradlebt@gmail.com>
2023-12-15 15:07:42 +0100

    move fetch to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const URL = "https://example-apis.vercel.app/api/art";
+
 
 export default function App({ Component, pageProps }) {
+
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+  console.log("data:", data);
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps}
+      pieces={isLoading || error ? [] : data} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
-import useSWR from "swr";
+/* import useSWR from "swr"; */
 import ArtPieces from "./ArtPieces";
 import Spotlight from "./Spotlight";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-const URL = "https://example-apis.vercel.app/api/art";
+/* const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const URL = "https://example-apis.vercel.app/api/art"; */
 
-export default function FetchArtPieces() {
-  const { data, error, isLoading } = useSWR(URL, fetcher);
-
+export default function FetchArtPieces({pieces}) {
+  
+/*   const { data, error, isLoading } = useSWR(URL, fetcher);
   console.log("data:", data);
-
   if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
+  if (isLoading) return <div>loading...</div>; */
 
-  const randomPicture = data[Math.floor(Math.random() * data.length)];
+  const randomPicture = pieces[Math.floor(Math.random() * pieces.length)];
 
   console.log("random ", randomPicture);
 
@@ -24,7 +23,7 @@ export default function FetchArtPieces() {
         artist={randomPicture.title}
       />
       <h1>Art Gallery</h1>
-      <ArtPieces pieces={data} />
+      <ArtPieces pieces={pieces} />
     </div>
   );
 }
