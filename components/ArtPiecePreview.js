9a7a77d9b1e529bcb57b6ea4b56a19c4965af05c
this.Konrad <konradlebt@gmail.com>
2023-12-17 21:38:07 +0100

    favourite-icon done

--- components/ArtPiecePreview.js
+++ components/ArtPiecePreview.js
@@ -1,17 +1,13 @@
 import ImageContainer from "./ImageContainer.js";
 
+
 export default function ArtPiecePreview( { title, image, artist } ) {
 
     return(<>
-        <div className ="name">
-            {title}
-        </div>
-        <div className="artist">
-            {artist}
-        </div>
         <ImageContainer 
         src={image}
         alt={title}
+        artist={artist}
         />
         </>)
 }
