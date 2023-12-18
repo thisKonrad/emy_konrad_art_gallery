b1865b2e330b97166fdf7ecd4b1dc9785729b2c8
this.Konrad <konradlebt@gmail.com>
2023-12-18 11:53:16 +0100

    spotlight with fav iucon

--- components/Spotlight.js
+++ components/Spotlight.js
@@ -1,11 +1,39 @@
 import Image from "next/image";
+import { useState } from "react";
 
 
-export default function Spotlight({ image, artist, title }) {
+export default function Spotlight({ image, artist, title}) {
+
+  const[favourite, setFavourite]= useState(true);
+  function handleToggle(){
+    setFavourite(!favourite)
+  }
+
   return (
     <div className="spotlight_wrap">
-      <Image src={image} alt={title} height={440} width={440}/>
-      <h2>Artist name: {artist}</h2>
+    <button className ="favourite_icon"
+     onClick={()=>handleToggle()}>{
+      favourite ?
+      <Image 
+      className ="clear_icon"
+      width={25}
+      height={25}
+      src='Heart_Clear.svg' 
+      alt='clear-heart'></Image> :
+     <Image 
+      width={25}
+      height={25}
+      src='Heart_Filled.svg' 
+      alt='filled-heart'></Image> }
+    </button>
+    <Image
+    src={image}
+    height={540}
+    width={540}
+    alt={title}/>
+    <div className='artwork_description'>
+    <h3 >Artist name: {artist}</h3>
+    <h4 >Title: {title}</h4>
     </div>
-  );
+  </div>);
 }
