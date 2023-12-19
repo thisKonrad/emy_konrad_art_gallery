d9e0841035facb5fa7f756f1ae03ccd527173387
this.Konrad <konradlebt@gmail.com>
2023-12-19 12:20:19 +0100

    defaultValue again

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
 
   const [artPiecesInfo, setArtPiecesInfo] = useLocalStorageState(
     "art-pieces-info",
-    { defaultvalue: [] }
+    { defaultValue: [] }
   );
 
   if (error) return <div>failed to load</div>;
